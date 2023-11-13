3f0baef78fb877071ba4a8b212c11bd4c972f279
p3zhy <pejman.ghd@gmail.com>
2023-11-13 14:42:42 +0330

    prepare structure of project

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ecu_parser.h"
+
+int main(int argc, char *argv[]) {
+    // TODO: Parse command-line arguments using getopt or similar
+
+    // TODO: Initialize ECU parser and process raw data
+
+    // TODO: Display results or error messages
+
+    return 0;
+}
