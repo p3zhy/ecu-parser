389001d5511d1480b725df1e1fd12a89df2d9380
p3zhy <pejman.ghd@gmail.com>
2023-11-14 16:12:49 +0330

    mode,id and data added to command line

---
+++ build/CMakeFiles/ecutool.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecutool.dir/src/ecu_parser.c.o"
+  "CMakeFiles/ecutool.dir/src/main.c.o"
+  "ecutool"
+  "ecutool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ecutool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
