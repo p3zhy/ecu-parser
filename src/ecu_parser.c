69988f8284348b504c81f9a04293fe2ba2c06fea
p3zhy <pejman.ghd@gmail.com>
2023-11-16 12:06:56 +0330

    Application Layer Protocol enum addded to project

--- src/ecu_parser.c
+++ src/ecu_parser.c
@@ -1,14 +1,17 @@
 #include "ecu_parser.h"
 
-
-void findProtocol(uint32_t id, unsigned char *data, int dataSize) {
+ApplicationLayerProtocol findProtocol(uint32_t id, unsigned char *data, int dataSize)
+{
     printf("Finding protocol with id: %u and data: ", id);
-    for (int i = 0; i < dataSize; ++i) {
+    for (int i = 0; i < dataSize; ++i)
+    {
         printf("%02u ", data[i]);
     }
     printf("\n");
+    return OBD2_APPLICATION_LAYER_PROTOCOL;
 }
 
-void parseProtocol() {
+void parseProtocol(uint32_t id, unsigned char *data, ApplicationLayerProtocol protocol)
+{
     printf("Parsing protocol...\n");
 }
--- src/ecu_parser.h
+++ src/ecu_parser.h
@@ -4,7 +4,19 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
-void findProtocol(uint32_t id, unsigned char *data, int dataSize);
-void parseProtocol();
+
+typedef enum ApplicationLayerProtocol
+{
+    J1939_APPLICATION_LAYER_PROTOCOL,
+    CANOPEN_APPLICATION_LAYER_PROTOCOL,
+    OBD2_APPLICATION_LAYER_PROTOCOL,
+    UDS_APPLICATION_LAYER_PROTOCOL,
+    LIN_APPLICATION_LAYER_PROTOCOL,
+    NMEA2000_APPLICATION_LAYER_PROTOCOL,
+    ISOBUS_APPLICATION_LAYER_PROTOCOL
+} ApplicationLayerProtocol;
+
+ApplicationLayerProtocol findProtocol(uint32_t id, unsigned char *data, int dataSize);
+void parseProtocol(uint32_t id, unsigned char *data, ApplicationLayerProtocol protocol);
 
 #endif // ECU_PARSER_H
